38ccef14b4080a6e1f6a0529391420d069fe0029
rodo <rodonatanael02@gmail.com>
2022-03-12 16:34:14 +0700

    menambahkan class KodeBuah

---
+++ KodeBuah.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3_1302204075
+{
+    internal class KodeBuah
+    {
+        public enum buah
+        {
+            Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+        }
+
+        public static string getKodeBuah(buah buah)
+        {
+            string[] KodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "M00", "N00", "O00" };
+
+            return KodeBuah[(int)buah];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Modul3_1302204075 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Apel));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Aprikot));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Alpukat));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Pisang));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Paprika));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Blackberry));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Ceri));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Kelapa));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Jagung));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Kurma));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Durian));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Anggur));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Melon));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.buah.Semangka));
+            Console.WriteLine("");
+
+
+
+        }
+    }
+}
